395f9d287c9ed85da84676db7b0893fd641c6784
Hasan KARAOĞLU <hkaraoglutr@gmail.com>
2019-08-19 22:10:03 +0300

    --> Unnecessary configuration was removed. --> process env variables were added as optional.

--- config.js
+++ config.js
@@ -1,21 +1,14 @@
 module.exports  = {
-
-    "db" :{
-        "mysql" : {
-            "host"     : "127.0.0.1",
-            "port"     : "3306",
-            "database" : "node-commerce",
-            "username" : "root",
-            "password" : "1234"
-        },
-        "mongo" : {
-            "host" : "localhost",
-            "port" : "27017",
-            "database" : "local"
+    "db" :
+        {
+            "mongo" : {
+                "host" : process.env.MONGODB_HOST ||  "localhost",
+                "port" : process.env.MONGODB_PORT ||  "27017",
+                "database" : process.env.MONGODB_DATABASE || "local"
         }
      },
     "session" :
     {
-        "secret_key" : "GJVt9fwBVPIDCHsN6D9E"
+        "secret_key" : process.env.SESSION_SECRET_KEY || "GJVt9fwBVPIDCHsN6D9E"
     }
 };
